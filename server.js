abe92792039686b0286fa298c6101370cca8ed9d
wsvnhub <blackstarche@gmail.com>
2024-08-21 14:41:18 +0700

    Update server.js ban dau

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const job = CronJob.from({
 
     const lastDate = new Date(
       new Date().getFullYear(),
-      new Date().getMonth() + 1,
+      new Date().getMonth() + 4,
       0
     ).getDate();
 
