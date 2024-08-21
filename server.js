34e541a323d3ff01dffaaab2b99ee666311a5e13
wsvnhub <blackstarche@gmail.com>
2024-08-21 14:01:57 +0700

    Update server.js L5

--- server.js
+++ server.js
@@ -37,19 +37,37 @@ const job = CronJob.from({
   onTick: async function () {
     console.log("You will see this message every second");
 
-    const lastDate = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth() + 1,
-      0
-    ).getDate();
+    const currentDate = new Date();
+let futureMonth = currentDate.getMonth() + 4;
+let futureYear = currentDate.getFullYear();
+
+if (futureMonth > 11) {
+  futureMonth -= 12;
+  futureYear += 1;
+}
+
+const lastDate = new Date(futureYear, futureMonth + 1, 0).getDate();
+
 
     let i = new Date().getDate();
 
     const courts = await mongoPool.collection("courts").find().toArray();
     console.log(i, lastDate);
-    while (i <= lastDate) {
-      const date = new Date();
-      date.setDate(i);
+    while (futureYear < currentDate.getFullYear() || (futureYear === currentDate.getFullYear() && futureMonth <= currentDate.getMonth() + 4)) {
+  const date = new Date(futureYear, futureMonth, i);
+  
+  if (i > lastDate) {
+    i = 1;
+    futureMonth++;
+    if (futureMonth > 11) {
+      futureMonth = 0;
+      futureYear++;
+    }
+    lastDate = new Date(futureYear, futureMonth + 1, 0).getDate();
+  } else {
+    i++;
+  }
+
       const insertData = courts.map((court) => {
         const timeslots = generateTimeArray(court.timeClusterId);
         return {
