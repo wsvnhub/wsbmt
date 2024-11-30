983714fedb34f0d8f84be6c288b78054c887cf31
llamas-laptrinh <danghoangson51@gmail.com>
2024-11-30 16:03:09 +0700

    updated UI

--- app/page.tsx
+++ app/page.tsx
@@ -341,18 +341,21 @@ export default function Home() {
   return (
     <>
       {isSchedule && (
-        <p className="hidden lg:block sticky top-0 left-0 z-40 bg-primary">
-          Để xem giờ tối: Nhấn giữ Shift và Scroll để cuộn ngang
-        </p>
+        <>
+          <p className="hidden lg:block sticky top-0 left-0 z-40 bg-primary">
+            Để xem giờ tối: Nhấn giữ Shift và Scroll để cuộn ngang
+          </p>
+          <Alert className="sticky top-6 left-0 z-40" banner message={
+            <Marquee pauseOnHover gradient={false}>
+              (Quý KH thuê sân để tổ chức giải hoặc ghi hình cần liên hệ 0389145575 trước, nếu không, Ways có quyền từ chối)
+            </Marquee>
+          } />
+        </>
       )}
-      <Alert className="sticky top-6 left-0 z-40" banner message={
-        <Marquee pauseOnHover gradient={false}>
-          (Quý KH thuê sân để tổ chức giải hoặc ghi hình cần liên hệ 0389145575 trước, nếu không, Ways có quyền từ chối)
-        </Marquee>
-      } />
+
       {contextHolder}
       <header
-        className={`${headerPadding} lg:sticky bg-primary top-8 flex flex-col lg:flex-row items-center lg:gap-4 gap-2 justify-between z-30`}
+        className={`${headerPadding} lg:sticky bg-primary ${isSchedule ? "top-8" : "top-0"} flex flex-col lg:flex-row items-center lg:gap-4 gap-2 justify-between z-30`}
       >
         <h1 className="text-lg lg:text-xl font-semibold my-2 text-center lg:text-left lg:mb-0">
           {isSchedule && (
