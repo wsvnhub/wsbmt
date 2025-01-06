f9792cbf9990e0df167d5115ea18252d0fb74c37
llamas-laptrinh <danghoangson51@gmail.com>
2025-01-06 11:32:03 +0700

    add light box

--- components/LightBox/index.tsx
+++ components/LightBox/index.tsx
@@ -1,7 +1,32 @@
+"use client"
 import React from 'react'
+import { Button, Modal } from 'antd';
 
-export default function index() {
+export default function LightBox() {
+  const [isModalOpen, setIsModalOpen] = React.useState(true);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>index</div>
-  )
+    <>
+      <Modal title="Vui lòng chọn ngày và chi nhánh bạn muốn đặt sân!"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
 }
